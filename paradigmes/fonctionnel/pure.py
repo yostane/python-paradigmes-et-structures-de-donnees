579ababe04217f12e174632a6afdd2e52802c658
yostane <yostane@yahoo.fr>
2019-10-06 01:53:02 +0200

    ajout d'un exemple de fonction pure

---
+++ paradigmes/fonctionnel/pure.py
@@ -0,0 +1,12 @@
+
+
+def calculerLeDoubleNonPure():
+    x = int(input("Entrez un nombre: "))
+    return x * 2
+
+
+x = int(input("Entrez un nombre: "))
+
+
+def calculerLeDoublePure(x):
+    return x * 2

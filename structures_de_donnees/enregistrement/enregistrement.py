3ebe402ca789438e90d0c1472256ac70f277c879
yostane <yostane@yahoo.fr>
2019-10-16 15:30:44 +0200

    petite correction

--- structures_de_donnees/enregistrement/enregistrement.py
+++ structures_de_donnees/enregistrement/enregistrement.py
@@ -1,8 +1,18 @@
 """ Les enregistrements en Python """
 # Calcul de moyenne
 
+
 class Etudiant:
-    modules = {"res": 3, "admin": 1.5, "bd": 1.5, "web": 1.5, "tn": 3, "ang": 3, "com": 2, "prog": 1.5}
+    modules = {
+        "res": 3,
+        "admin": 1.5,
+        "bd": 1.5,
+        "web": 1.5,
+        "tn": 3,
+        "ang": 3,
+        "com": 2,
+        "prog": 1.5,
+    }
 
     def __init__(self, num, prenom, nom):
         assert isinstance(num, int) and num > 0
@@ -22,7 +32,7 @@ class Etudiant:
         self.notes[module] = note
 
     def moyenne(self):
-        res = 0.03
+        res = 0.0
         for m in Etudiant.modules.keys():
             res = res + (self.notes[m] * Etudiant.modules[m])
 
@@ -31,15 +41,15 @@ class Etudiant:
 
 
 # programme principal
-etud1 = Etudiant(1, "John", "Doe")
+etud1 = Etudiant(1, "Couette", "Dodo")
 
-etud1.add_note('res', 12.0)
-etud1.add_note('admin', 11)
-etud1.add_note('bd', 17)
-etud1.add_note('web', 16.5)
-etud1.add_note('tn', 9)
-etud1.add_note('ang', 13.5)
-etud1.add_note('com', 10)
-etud1.add_note('prog', 15.5)
+etud1.add_note("res", 12.0)
+etud1.add_note("admin", 11)
+etud1.add_note("bd", 17)
+etud1.add_note("web", 16.5)
+etud1.add_note("tn", 9)
+etud1.add_note("ang", 13.5)
+etud1.add_note("com", 10)
+etud1.add_note("prog", 15.5)
 
-print('> Moyenne de ', etud1.prenom, etud1.nom, ' : ', etud1.moyenne())
+print("> Moyenne de ", etud1.prenom, etud1.nom, " : ", etud1.moyenne())
